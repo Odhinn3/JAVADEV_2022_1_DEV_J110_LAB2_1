c2b6f76fe7c2bf77fae41b9963dcb93a457fc397
user <user@PC2060>
2022-03-29 12:38:38 +0300

    LAB 2 part 1 is complete

--- Lab2_1/src/MainPackage/Person.java
+++ Lab2_1/src/MainPackage/Person.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MainPackage;
 
-/**
- *
- * @author user
- */
+
 public class Person {//класс персон
     protected String name;//имя и фамилия
     protected enum Gend {MALE, FEMALE}//пол
