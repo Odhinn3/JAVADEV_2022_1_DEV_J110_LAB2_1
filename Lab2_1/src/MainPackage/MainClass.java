2726675d0e69e5858c45d950af5922f410ed0d2f
user <user@PC2060>
2022-03-29 12:36:20 +0300

    LAB 2 part 1 is complete

---
+++ Lab2_1/src/MainPackage/MainClass.java
@@ -0,0 +1,34 @@
+package MainPackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //массив преподавателей и студентов вуза
+        Person univArr[] = new Person[6];
+        univArr[0] = new Teacher("Ronald Turner", Teacher.Gend.MALE, "Computer science", Teacher.Degr.PHD, "Programming paradigms");
+        univArr[1] = new Teacher("Ruth Hollings", Teacher.Gend.FEMALE, "Jurisprudence", Teacher.Degr.MSC, "Domestic arbitration");
+        univArr[2] = new Student("Leo Wilkinson", Student.Gend.MALE, "Computer science", Student.Stage.BACHELOR, 3);
+        univArr[3] = new Student("Anna Cunningham", Student.Gend.FEMALE, "World economics", Student.Stage.BACHELOR, 1);
+        univArr[4] = new Student("Jill Lundqvist", Student.Gend.FEMALE, "Jurisprudence", Student.Stage.MASTER, 1);
+        univArr[5] = new Student("Ronald Correa", Student.Gend.MALE, "Computer science", "Design of a functional programming language");
+        
+        //вывод на печать
+        Person.printAll(univArr);
+
+    }
+    
+}
